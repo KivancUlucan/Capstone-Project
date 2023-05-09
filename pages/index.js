b298a86b792fcffb27442949f9268dcb1f230871
Kivanc Ulucan <kulucan@gmx.de>
2023-05-09 12:15:51 +0200

    starting Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import styled from "styled-components";
-
 export default function HomePage() {
-  return;
+  return <h1>Summary</h1>;
 }
